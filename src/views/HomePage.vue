abbbe5f04a76269bb0ea3a8f334a1c2a125792b9
nawihusen <nawihusen@gmail.com>
2023-08-03 09:39:15 +0700

    git init

---
+++ src/views/HomePage.vue
@@ -0,0 +1,8 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <p class="text-red-600">NOTHING Banyalk</p>
+  </main> 
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: 'class', // or 'media' or 'class',
+  theme: {
+    extend: {}
+  },
+  plugins: []
+}
