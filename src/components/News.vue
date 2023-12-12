3ab7a41f137cef6a69827aa5c443e4a53c58e2f0
nawihusen <nawihusen@gmail.com>
2023-12-12 22:12:43 +0700

    camuh

--- src/components/News.vue
+++ src/components/News.vue
@@ -24,7 +24,7 @@ const props = defineProps({
 <template>
   <div>
     <div>
-      <img :src="props.imgUrl" class="rounded w-40 h-40" alt="gambar berita" />
+      <img :src="props.imgUrl" class="rounded w-32 h-32" alt="gambar berita" />
       <p class="p-0 text-xs font-semibold text-red-500">{{ props.time }}</p>
     </div>
     <p>{{ props.title }}</p>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -19,24 +19,127 @@ const temps = [
     title: 'Judul 2',
     img: 'src/assets/images/default.jpg'
   },
-  {}
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  },
+  {
+    time: 'senin jalo hala',
+    title: 'Judul 1',
+    img: 'src/assets/images/kazuha.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/news.png'
+  },
+  {
+    time: 'selasa jalo hala',
+    title: 'Judul 2',
+    img: 'src/assets/images/default.jpg'
+  }
 ]
 </script>
 
 <template>
-  <div class="flex flex-row w-full h-full fixed">
+  <div class="flex flex-row w-full h-full">
     <div>
       <sideDrawer />
     </div>
 
     <div class="flex grow flex-col">
       <div><navbar /></div>
-
-      <div class="flex h-full items-center justify-center border-black border-8 p-8">
+      <div class="border-black border-8 p-8">
         <div
-          class="flex h-full w-full flex-wrap sm:bg-red-500 md:bg-orange-500 lg:bg-yellow-500 xl:bg-green-500 2xl:bg-blue-500"
+          class="flex h-full w-full sm:bg-red-500 md:bg-orange-500 lg:bg-yellow-500 xl:bg-green-500 2xl:bg-blue-500"
         >
-          <ul class="flex flex-row gap-3">
+          <ul class="flex overflow-auto flex-row flex-wrap gap-3 bg-black w-4/5">
             <li class="test" v-for="(item, i) in temps" :key="i">
               <news :time="item.time" :title="item.title" :imgUrl="item.img" />
             </li>
