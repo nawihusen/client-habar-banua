2ebac54ed624f8e246d5f08d51e95e5d1ee6a02d
nawihusen <nawihusen@gmail.com>
2023-12-28 20:26:57 +0700

    commit

--- src/components/MyNews.vue
+++ src/components/MyNews.vue
@@ -107,9 +107,9 @@ const temps = [
 </script>
 
 <template>
-  <div><h4 class="p8">My News</h4></div>
+  <div><h4 class="p8 bg-gray-400 w-fit px-4 rounded-tr-full">My News</h4></div>
   <div>
-    <ul class="flex flex-row flex-wrap bg-gray-400 rounded-md justify-between gap-y-2 gap-x-2 p-4">
+    <ul class="flex flex-row flex-wrap bg-gray-400 justify-between gap-y-2 gap-x-2 p-4">
       <li class="bg-white" v-for="(item, index) in temps" :key="index">
         <news :time="item.time" :title="item.title" :imgUrl="item.img" />
       </li>
