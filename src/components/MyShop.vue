fbefa183a1534b2bc6ea12c376ea4e1724371a62
nawihusen <nawihusen@gmail.com>
2023-12-29 21:07:13 +0700

    commit

--- src/components/MyShop.vue
+++ src/components/MyShop.vue
@@ -7,5 +7,12 @@ defineComponent({
 </script>
 
 <template>
-  <div><p>shop</p></div>
+  <div><h4 class="p8 bg-gray-400 w-fit px-4 rounded-tr-full">My Shop</h4></div>
+  <div>
+    <ul class="flex flex-row flex-wrap bg-gray-400 justify-between gap-y-2 gap-x-2 p-4">
+      <li class="bg-white" v-for="(item, index) in temps" :key="index">
+        <news :time="item.time" :title="item.title" :imgUrl="item.img" />
+      </li>
+    </ul>
+  </div>
 </template>
