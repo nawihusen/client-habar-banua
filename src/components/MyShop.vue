5bbc5a4e7edcff87a4b5a4bde944fdceba2bc974
nawihusen <nawihusen@gmail.com>
2024-01-18 21:29:34 +0700

    commit

--- src/components/MyShop.vue
+++ src/components/MyShop.vue
@@ -1,17 +1,31 @@
 <script setup>
 import { defineComponent } from 'vue'
+import shop from '@/components/ShopCard.vue'
 
 defineComponent({
   name: 'MyShopComponent'
 })
+
+const items = [
+  {
+    id: 1,
+    name: 'Produk 1',
+    price: 20000
+  },
+  {
+    id: 2,
+    name: 'Produk 2',
+    price: 50000
+  }
+]
 </script>
 
 <template>
   <div><h4 class="p8 bg-gray-400 w-fit px-4 rounded-tr-full">My Shop</h4></div>
   <div>
     <ul class="flex flex-row flex-wrap bg-gray-400 justify-between gap-y-2 gap-x-2 p-4">
-      <li class="bg-white" v-for="(item, index) in temps" :key="index">
-        <news :time="item.time" :title="item.title" :imgUrl="item.img" />
+      <li class="bg-white" v-for="(item, index) in items" :key="index">
+        <shop :name="item.name" :price="item.price" :id="item.id" />
       </li>
     </ul>
   </div>
---
+++ src/components/ShopCard.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { defineComponent } from 'vue'
+
+defineComponent({
+  name: 'ShopCardComponent'
+})
+
+// function shorter(data) {
+//   if (data.length > 60) {
+//     return data.substring(0, 45) + '. . .'
+//   } else if (data.length <= 60) {
+//     return data
+//   }
+// }
+
+const props = defineProps({
+  imgUrl: {
+    type: String,
+    default: 'src/assets/images/news.png'
+  },
+  id: {
+    type: Number,
+    default: 0
+  },
+  name: {
+    type: String,
+    default: ''
+  },
+  price: {
+    type: Number,
+    default: 0
+  }
+})
+</script>
+
+<template>
+  <div class="h-44 w-44 p-2">
+    <div class="flex flex-col justify-center">
+      <div class="flex h-full w-full items-center justify-center">
+        <img :src="props.imgUrl" class="rounded w-24 h-24" alt="gambar berita" />
+      </div>
+    </div>
+    <div class="flex flex-row flex-wrap w-full">
+      <div class="flex flex-col">
+        <p class="tag"><span>Name</span>:</p>
+        <p class="tag"><span>Price</span>:</p>
+      </div>
+      <div class="flex flex-col">
+        <p class="tag ml-1">{{ name }}</p>
+        <p class="tag ml-1">{{ price }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.tag {
+  font-size: small;
+}
+</style>
