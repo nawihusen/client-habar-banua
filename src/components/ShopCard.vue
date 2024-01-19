a0a1dfaf3370ed9d5667c1e2759bfa538dfff6f8
nawihusen <nawihusen@gmail.com>
2024-01-19 21:43:38 +0700

    commit

--- src/components/ShopCard.vue
+++ src/components/ShopCard.vue
@@ -1,17 +1,20 @@
 <script setup>
 import { defineComponent } from 'vue'
+// import { defineComponent, ref } from 'vue'
+// import { useRouter } from 'vue-router'
 
 defineComponent({
   name: 'ShopCardComponent'
 })
 
-// function shorter(data) {
-//   if (data.length > 60) {
-//     return data.substring(0, 45) + '. . .'
-//   } else if (data.length <= 60) {
-//     return data
-//   }
-// }
+// const router = useRouter()
+
+// const form = ref({
+//   imgUrl: '',
+//   id: '',
+//   name: '',
+//   price: ''
+// })
 
 const props = defineProps({
   imgUrl: {
@@ -31,16 +34,20 @@ const props = defineProps({
     default: 0
   }
 })
+
+const edit = (id) => {
+  console.log(id)
+}
 </script>
 
 <template>
-  <div class="h-44 w-44 p-2">
+  <div class="h-44 w-44 p-2" @click="edit(id)">
     <div class="flex flex-col justify-center">
       <div class="flex h-full w-full items-center justify-center">
         <img :src="props.imgUrl" class="rounded w-24 h-24" alt="gambar berita" />
       </div>
     </div>
-    <div class="flex flex-row flex-wrap w-full">
+    <div class="flex flex-row flex-wrap w-full mt-4 pl-2">
       <div class="flex flex-col">
         <p class="tag"><span>Name</span>:</p>
         <p class="tag"><span>Price</span>:</p>
